70950977657a0b43e68934a6e27b14f2122fac45
Otoniel <ohernandezgarzon@gmail.com>
2018-06-18 15:51:28 -0500

    configuracion nueva del paquete

--- Game/js/statusGame/principal.js
+++ Game/js/statusGame/principal.js
@@ -11,14 +11,14 @@
             Game.scale.pageAlignVertically = true;
         },
         create:()=>{
-            // MOSTRSR EN PANTALLA 
+            // MOSTRAR EN PANTALLA 
         // cuerpo del tanque   
-            cañon=Game.add.graphics(0,393.5);
-            cañon.beginFill(0xc5c5c5c5);
-            cañon.lineStyle(10,0Xc0c0c0,2);
-            cañon.drawRect(60,0,130,20);
-            cañon.moveTo(60,0)
-            cañon.endFill();
+            canon=Game.add.graphics(0,393.5);
+            canon.beginFill(0xc5c5c5c5);
+            canon.lineStyle(10,0Xc0c0c0,2);
+            canon.drawRect(60,0,130,20);
+            canon.moveTo(60,0);
+            canon.endFill();
             
             // cuerpo del tanque   
             cuerpo = Game.add.graphics(0,0);
@@ -33,22 +33,22 @@
             izquierda = Game.input.keyboard.createCursorKeys();
             arriba = Game.input.keyboard.createCursorKeys();
             abajo = Game.input.keyboard.createCursorKeys();
-            // limintes del carco en canvas
+            // limites del cargo en canvas
             Game.physics.startSystem(Phaser.Physics.ARCADE);
             Game.physics.arcade.enable(cuerpo);
-            Game.physics.arcade.enable(cañon);
-            cañon.body.collideWorldBounds =true;
-            // cañon.anchor.setTo(1);
+            Game.physics.arcade.enable(canon);
+            canon.body.collideWorldBounds =true;
+            // canon.anchor.setTo(1);
             
             cuerpo.body.collideWorldBounds =true;
            },
         update:()=>{
             // animación del juego;
-           if (derecha.right.isDown) {cañon.position.x +=3 ; cuerpo.position.x +=3}
-           if (izquierda.left.isDown) {cañon.position.x -=3; cuerpo.position.x -=3}
-           if (arriba.up.isDown) cañon.angle -= 1 && cañon.angle >= -8.5  ;
-           if (arriba.down.isDown) cañon.angle +=1 && cañon.angle <= 5.5;
+           if (derecha.right.isDown) {canon.position.x +=3 ; cuerpo.position.x +=3;}
+           if (izquierda.left.isDown) {canon.position.x -=3; cuerpo.position.x -=3;}
+           if (arriba.up.isDown) canon.angle -= 1 && canon.angle >= -8.5  ;
+           if (arriba.down.isDown) canon.angle +=1 && canon.angle <= 5.5;
         }
     }; 
-Game.state.add('firts', statusFirst);
-Game.state.start('firts');
+Game.state.add('firths', statusFirst);
+Game.state.start('firths');
--- programar/gulpFile.js
+++ programar/gulpFile.js
@@ -3,10 +3,10 @@ const gulp  = require('gulp'),
       style = require('gulp-stylus'),
       error = require('gulp-plumber'),
       reload = require('browser-sync').create(),
-      jsError = require('gulp-jslint');
+      jsError = require('gulp-jshint');
 
 // Crear el html
-gulp.task('html', () => {
+gulp.task('html', function() {
     gulp.src('./pug/*.pug')
         .pipe(error())
         .pipe(html({
@@ -16,17 +16,17 @@ gulp.task('html', () => {
         .pipe(gulp.dest('../Game/'))
         .pipe(reload.stream());    
 });
-// marquetacióny diseño
-gulp.task('css', () => {
-    gulp.src('./styles/*.styl')
+//  Diseño de la pagina
+gulp.task('css', function() {
+    gulp.src('./styles/*.stylus')
         .pipe(error())
         .pipe(style())
         .pipe(error.stop())
         .pipe(gulp.dest('../Game/css'))
-        .pipe(reload.stream())
+        .pipe(reload.stream());
 }); 
-// Recagar el proyecto con nuevos cambio
-gulp.task('Sync', ()=>{
+// Recargar el proyecto con nuevos cambio
+gulp.task('Sync', function (){
     reload.init({
         server: {
             baseDir: "../Game"
@@ -35,27 +35,28 @@ gulp.task('Sync', ()=>{
 });
 
 //Corregir Javascript
-gulp.task('js',()=>{
+gulp.task('js',function (){
     gulp.src('./js/*.js')
         .pipe(error())
+        .pipe(jsError({esversion:6}))
+        .pipe(jsError.reporter('default')) 
         .pipe(error.stop())
         .pipe(gulp.dest('../Game/js/statusGame/'))
         .pipe(reload.stream());
 });
 // Corregir a gulp
-gulp.task('gulp', ()=> {
+gulp.task('gulp', function () {
     gulp.src('./gulpFile.js')
         .pipe(error())
-        .pipe(jsError())
+        .pipe(jsError({esversion:6}))
         .pipe(jsError.reporter('default'))
         .pipe(error.stop())
-        .pipe(reload.stream())
+        .pipe(reload.stream());
 });
-
 // Ver, automatizar y actualizar las tareas
 
 gulp.watch('./pug/*.pug', ['html']);
-gulp.watch('./styles/*.styl', ['css']);
+gulp.watch('./styles/*.stylus', ['css']);
 gulp.watch('./js/*.js', ['js']);
 gulp.watch('./gulpFile.js', ['gulp']);
 gulp.task('default', ['html', 'css', 'Sync', 'js', 'gulp']);
